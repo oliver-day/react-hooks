e2307cdaf2dcf456578d2f48623397bdeba2e0f4
Oliver Day <oliver.day.swe@gmail.com>
2023-01-16 17:30:41 -0800

    feat: add 'set state in react' solution

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,14 +1,13 @@
 // useState: greeting
 // http://localhost:3000/isolated/exercise/01.js
 
-import * as React from 'react'
+import * as React from 'react';
 
 function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const [name, setName] = React.useState('');
 
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+    setName(event.target.value);
   }
 
   return (
@@ -19,11 +18,11 @@ function Greeting() {
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
-  )
-}
+  );
+};
 
 function App() {
   return <Greeting />
-}
+};
 
-export default App
+export default App;
