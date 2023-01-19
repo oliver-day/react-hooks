eab33647fa80bab4ab2798d5aed1a49cc136b0e8
Oliver Day <oliver.day.swe@gmail.com>
2023-01-18 16:01:48 -0800

    feat: add 'useLocalStorageState custom hook' solution

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,16 +2,11 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react';
+import { useLocalStorageState } from '../utils';
 
 function Board() {
   // 🐨 squares is the state for this component.
-  const [squares, setSquares] = React.useState(
-    () => JSON.parse(window.localStorage.getItem('squares')) || Array(9).fill(null)
-    );
-
-  React.useEffect(() => {
-    window.localStorage.setItem('squares', JSON.stringify(squares));
-  }, [squares]);
+  const [squares, setSquares] = useLocalStorageState('squares', Array(9).fill(null));
 
   const nextValue = calculateNextValue(squares);
   const winner = calculateWinner(squares);
